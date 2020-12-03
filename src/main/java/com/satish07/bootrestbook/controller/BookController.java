187714cda163d02e27aef43f5661ff0c7d9ef43b
Satish Kumar <satish.kumar06@nagarro.com>
2020-12-03 19:28:23 +0530

    Handling HttpStatus while creating REST API
    Signed-off-by: Satish Kumar <satish.kumar06@nagarro.com>

--- src/main/java/com/satish07/bootrestbook/controller/BookController.java
+++ src/main/java/com/satish07/bootrestbook/controller/BookController.java
@@ -1,8 +1,11 @@
 package com.satish07.bootrestbook.controller;
 
 import java.util.List;
+import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -21,30 +24,56 @@ public class BookController {
 	private BookService bookService;
 	
 	@GetMapping("/books")
-	public List<Book> getBooks() {
-		return this.bookService.getAllBooks();
+	public ResponseEntity<List<Book>> getBooks() {
+		List<Book> list = bookService.getAllBooks();
+		if(list.size()<=0) {
+			return ResponseEntity.status(HttpStatus.NOT_FOUND).build();
+		}
+		return ResponseEntity.of(Optional.of(list));
 	}
 	
 	@GetMapping("/books/{id}")
-	public Book getBook(@PathVariable("id") int id) {
-		return bookService.getBookById(id);
+	public ResponseEntity<Book> getBook(@PathVariable("id") int id) {
+		Book book = bookService.getBookById(id);
+		if(book==null) {
+			return ResponseEntity.status(HttpStatus.NOT_FOUND).build();
+		}
+		return ResponseEntity.of(Optional.of(book));
 	}
 	
 	@PostMapping("/books")
-	public Book addBook(@RequestBody Book book) {
-		Book b = this.bookService.addBook(book);
-		System.out.println(book);
-		return b;
+	public ResponseEntity<Book> addBook(@RequestBody Book book) {
+		Book b = null;
+		try {
+			b = this.bookService.addBook(book);
+			System.out.println(book);
+			return ResponseEntity.of(Optional.of(b));
+		} catch (Exception e) {
+			e.printStackTrace();
+			return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).build();
+		}
 	}
 	
 	@DeleteMapping("/books/{bookId}")
-	public void deleteBook(@PathVariable("bookId") int bookId) {
-		this.bookService.deleteBook(bookId);
+	public ResponseEntity<Void> deleteBook(@PathVariable("bookId") int bookId) {
+		try {
+			this.bookService.deleteBook(bookId);
+			return ResponseEntity.status(HttpStatus.NO_CONTENT).build();
+		} catch (Exception e) {
+			e.printStackTrace();
+			return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).build();
+		}
+		
 	}
 	
 	@PutMapping("/books/{bookId}")
-	public Book updateBook(@RequestBody Book book,@PathVariable("bookId") int bookId) {
-		this.bookService.updateBook(book,bookId);
-		return book;
+	public ResponseEntity<Book> updateBook(@RequestBody Book book,@PathVariable("bookId") int bookId) {
+		try {
+			this.bookService.updateBook(book,bookId);
+			return ResponseEntity.ok().body(book);
+		} catch (Exception e) {
+			e.printStackTrace();
+			return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).build();
+		}
 	}
 }
--- src/main/java/com/satish07/bootrestbook/services/BookService.java
+++ src/main/java/com/satish07/bootrestbook/services/BookService.java
@@ -28,7 +28,12 @@ public class BookService {
 	//get single book by id
 	public Book getBookById(int id) {
 		Book book = null;
-		book = list.stream().filter(e->e.getId() == id).findFirst().get();
+		try {
+			book = list.stream().filter(e->e.getId() == id).findFirst().get();
+		} catch (Exception e2) {
+			e2.printStackTrace();
+		}
+		
 		return book;
 	}
 	
