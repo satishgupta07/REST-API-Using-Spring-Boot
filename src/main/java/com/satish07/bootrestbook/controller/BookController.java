437afcc51e5d558a64b8d7f04045a155db706643
Satish Kumar <satish.kumar06@nagarro.com>
2020-12-03 17:52:00 +0530

    Created Get functionality
    Signed-off-by: Satish Kumar <satish.kumar06@nagarro.com>

---
+++ src/main/java/com/satish07/bootrestbook/controller/BookController.java
@@ -0,0 +1,37 @@
+package com.satish07.bootrestbook.controller;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.satish07.bootrestbook.entities.Book;
+import com.satish07.bootrestbook.services.BookService;
+
+@RestController
+public class BookController {
+	
+	@Autowired
+	private BookService bookService;
+
+//	@GetMapping("/books")
+//	public Book getBooks() {
+//		Book book = new Book();
+//		book.setId(12324);
+//		book.setTitle("Java Complete Reference");
+//		book.setAuthor("XYZ");
+//		return book;
+//	}
+	
+	@GetMapping("/books")
+	public List<Book> getBooks() {
+		return this.bookService.getAllBooks();
+	}
+	
+	@GetMapping("/books/{id}")
+	public Book getBook(@PathVariable("id") int id) {
+		return bookService.getBookById(id);
+	}
+}
---
+++ src/main/java/com/satish07/bootrestbook/services/BookService.java
@@ -0,0 +1,34 @@
+package com.satish07.bootrestbook.services;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import org.springframework.stereotype.Component;
+
+import com.satish07.bootrestbook.entities.Book;
+
+@Component
+public class BookService {
+	
+	private static List<Book> list = new ArrayList<>();
+	
+	static {
+		list.add(new Book(12,"Java Complete Reference", "XYZ"));
+		list.add(new Book(36,"Head First Java", "ABC"));
+		list.add(new Book(48,"Think in Java", "LMN"));
+		list.add(new Book(64,"Python", "DFC"));
+	}
+	
+	//get all books
+	public List<Book> getAllBooks() {
+		return list;
+	}
+	
+	//get single book by id
+	public Book getBookById(int id) {
+		Book book = null;
+		book = list.stream().filter(e->e.getId() == id).findFirst().get();
+		return book;
+	}
+
+}
