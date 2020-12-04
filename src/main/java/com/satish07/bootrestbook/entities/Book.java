7ca979f7954adc11d5225c35172322630e53c71b
Satish Kumar <satish.kumar06@nagarro.com>
2020-12-04 14:14:10 +0530

    Nesting Custom Type | Using OneToOne Mapping
    Signed-off-by: Satish Kumar <satish.kumar06@nagarro.com>

--- src/main/java/com/satish07/bootrestbook/entities/Book.java
+++ src/main/java/com/satish07/bootrestbook/entities/Book.java
@@ -1,10 +1,12 @@
 package com.satish07.bootrestbook.entities;
 
+import javax.persistence.CascadeType;
 import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.OneToOne;
 import javax.persistence.Table;
 
 @Entity
@@ -16,39 +18,46 @@ public class Book {
 	@Column(name="book_id")
 	private int id;
 	private String title;
-	private String author;
+	
+	@OneToOne(cascade = CascadeType.ALL)
+	private Author author;
 	
 	public Book() {
 		super();
 		// TODO Auto-generated constructor stub
 	}
-	
-	public Book(int id, String title, String author) {
+
+	public Book(int id, String title, Author author) {
 		super();
 		this.id = id;
 		this.title = title;
 		this.author = author;
 	}
-	
+
 	public int getId() {
 		return id;
 	}
+
 	public void setId(int id) {
 		this.id = id;
 	}
+
 	public String getTitle() {
 		return title;
 	}
+
 	public void setTitle(String title) {
 		this.title = title;
 	}
-	public String getAuthor() {
+
+	public Author getAuthor() {
 		return author;
 	}
-	public void setAuthor(String author) {
+
+	public void setAuthor(Author author) {
 		this.author = author;
 	}
-	
+
 	@Override
 	public String toString() {
 		return "Book [id=" + id + ", title=" + title + ", author=" + author + "]";
