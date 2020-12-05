1fec2b5a96aed101c26b039c4e6dbecf786cae20
Satish Kumar <satish.kumar06@nagarro.com>
2020-12-05 12:14:52 +0530

    File Uploading Using Spring Boot REST API
    Signed-off-by: Satish Kumar <satish.kumar06@nagarro.com>

---
+++ src/main/java/com/satish07/bootrestbook/helper/FileUploadHelper.java
@@ -0,0 +1,47 @@
+package com.satish07.bootrestbook.helper;
+
+import java.io.File;
+//import java.io.FileOutputStream;
+//import java.io.InputStream;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+import java.nio.file.StandardCopyOption;
+
+import org.springframework.stereotype.Component;
+import org.springframework.web.multipart.MultipartFile;
+
+@Component
+public class FileUploadHelper {
+
+	public final String UPLOAD_DIR = "C:\\Users\\satishkumar06\\eclipse-workspace\\"
+		            	+ "bootrestbook\\src\\main\\resources\\static\\image";
+	
+	public boolean uploadFile(MultipartFile multipartFile) {
+		boolean f = false;
+		
+		try {
+			
+//			InputStream is = multipartFile.getInputStream();
+//			byte[] data = new byte[is.available()];
+//			is.read();
+//			
+//			//write
+//			FileOutputStream fos = new FileOutputStream(UPLOAD_DIR+File.separator+multipartFile.getOriginalFilename());
+//			fos.write(data);
+//			
+//			fos.flush();
+//			fos.close();
+			
+			Files.copy(multipartFile.getInputStream(),
+					   Paths.get(UPLOAD_DIR+File.separator+multipartFile.getOriginalFilename()),
+					   StandardCopyOption.REPLACE_EXISTING);
+			
+			f = true;
+			
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+		
+		return f;
+	}
+}
