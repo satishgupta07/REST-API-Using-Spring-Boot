c71416559a74aa9f5d0c1771785b80bf22047899
Satish Kumar <satish.kumar06@nagarro.com>
2020-12-05 17:27:37 +0530

    Image Uploading to dynamic path
    Signed-off-by: Satish Kumar <satish.kumar06@nagarro.com>

--- src/main/java/com/satish07/bootrestbook/helper/FileUploadHelper.java
+++ src/main/java/com/satish07/bootrestbook/helper/FileUploadHelper.java
@@ -1,20 +1,28 @@
 package com.satish07.bootrestbook.helper;
 
 import java.io.File;
+import java.io.IOException;
 //import java.io.FileOutputStream;
 //import java.io.InputStream;
 import java.nio.file.Files;
 import java.nio.file.Paths;
 import java.nio.file.StandardCopyOption;
 
+import org.springframework.core.io.ClassPathResource;
 import org.springframework.stereotype.Component;
 import org.springframework.web.multipart.MultipartFile;
 
 @Component
 public class FileUploadHelper {
 
-	public final String UPLOAD_DIR = "C:\\Users\\satishkumar06\\eclipse-workspace\\"
-		            	+ "bootrestbook\\src\\main\\resources\\static\\image";
+//	public final String UPLOAD_DIR = "C:\\Users\\satishkumar06\\eclipse-workspace\\"
+//		            	+ "bootrestbook\\src\\main\\resources\\static\\image";
+	
+	public final String UPLOAD_DIR = new ClassPathResource("static/image/").getFile().getAbsolutePath();
+
+	public FileUploadHelper() throws IOException {
+		
+	}
 	
 	public boolean uploadFile(MultipartFile multipartFile) {
 		boolean f = false;
